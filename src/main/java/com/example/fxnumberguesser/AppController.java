77491b0207339db04465c98b840fa028c8f7c31b
Hudson <LordHudsonThe1st@Gmail.com>
2023-08-20 11:05:08 +0200

    Added css styles and added guess messages to the GUI

--- src/main/java/com/example/fxnumberguesser/AppController.java
+++ src/main/java/com/example/fxnumberguesser/AppController.java
@@ -5,10 +5,14 @@ import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
 
 import java.util.Random;
 
 public class AppController {
+
+    @FXML
+    private AnchorPane anchorPane;
 
     @FXML
     private Button button;
@@ -17,41 +21,52 @@ public class AppController {
     private TextField number;
 
     @FXML
-    private Label score, highScore;
+    private Label score, highScore, message;
     private final Random random = new Random();
-    private int userNumber, originalScore = 20, randomNumber = random.nextInt(1,21);
+    private int originalScore = 20, randomNumber = random.nextInt(1,21);
 
     @FXML
     protected void onButtonClick() {
-        String mainScore = "Score: x";
-        String highScore = "High Score: x";
+        String mainScore = "\uD83D\uDCAF Score: x";
+        String highScore = "\uD83E\uDD47 High Score: x";
+        String noNumberMessage = "⛔ No number entered!";
+        String tooLow = "\uD83D\uDCC9 Too low!";
+        String tooHigh = "\uD83D\uDCC8 Too high!";
+        String correctScore = "\uD83C\uDF89 Correct Number!";
+        String gameOver = "\uD83D\uDCA5 You lost the game";
         try{
-           userNumber = Integer.parseInt(number.getText());
-           if (userNumber ==randomNumber){
-               setScore(highScore, originalScore, this.highScore);
-               button.setDisable(true);
-               System.out.println("Correct Guess: ");
+            int userNumber = Integer.parseInt(this.number.getText());
+           if (userNumber == this.randomNumber){
+               setScore(highScore, this.originalScore, this.highScore);
+               this.message.setText(correctScore);
+               this.anchorPane.setStyle("-fx-background-color:#60b347;");
+               this.button.setDisable(true);
            } else {
-               System.out.println("Incorrect Guess, Try again");
                originalScore--;
+               String msg = (userNumber < this.randomNumber ? ((this.originalScore == 0) ? gameOver:tooLow):tooHigh);
+               this.message.setText(msg);
+               if (this.message.getText().equals(gameOver)){
+                   this.button.setDisable(true);
+               }
            }
        } catch (NumberFormatException e){
+            this.message.setText(noNumberMessage);
            dialogBox();
-           System.out.println(e.getMessage());
        }
-        System.out.println("Random Number: " + randomNumber + ", user Number " + userNumber);
-        number.setText("");
-        setScore(mainScore, originalScore, this.score);
+        this.number.setText("");
+        setScore(mainScore, this.originalScore, this.score);
     }
 
     @FXML
     protected void reset(){
         this.number.setText("");
-        this.score.setText("Score: 0");
-        this.highScore.setText("High Score: 0");
+        this.score.setText("\uD83D\uDCAF Score: 0");
+        this.highScore.setText("\uD83E\uDD47 High Score: 0");
         this.originalScore = 20;
         this.randomNumber = random.nextInt(1,21);
         this.button.setDisable(false);
+        this.anchorPane.setStyle("-fx-background-color:#222;");
+        this.message.setText("Start Guessing...");
     }
     private void dialogBox(){
         Alert alert = new Alert(Alert.AlertType.ERROR);
--- src/main/java/com/example/fxnumberguesser/NumberGuesser.java
+++ src/main/java/com/example/fxnumberguesser/NumberGuesser.java
@@ -6,14 +6,17 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.Objects;
 
 public class NumberGuesser extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(NumberGuesser.class.getResource("num-guesser-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
+        scene.getStylesheets().add(Objects.requireNonNull(getClass().getResource("style.css")).toExternalForm());
         stage.setTitle("Number Guessing Game");
         stage.setScene(scene);
+        stage.setResizable(false);
         stage.show();
     }
 
